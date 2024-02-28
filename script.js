a3741a110ecf0287bdae74fa042f58eb36614211
michy0902 <michelle.kolawole13@gmail.com>
2024-02-28 15:26:11 +0100

    made the loop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //we create a date object and set the time to the day we want it to count down to
 //getTime is to get the timestamp in milliseconds 
-var countDownDate = new Date("Mar 15, 2024").getTime();
+var countDownDate = new Date("Mar 3, 2024").getTime();
 //we set an interval that is executed every one second(1000 millisecond)
 var x = setInterval(function(){
     // this collects the curent date and time and assigns it to the variable now
@@ -19,13 +19,13 @@ var x = setInterval(function(){
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
     // when the countdown is done, it'll stop it from counting backwards and just stay at 0
-    if(distance < 0){
-        clearInterval(x);
-        document.getElementById("days").innerHTML = "00";
+    if (distance < 0) {
+        setInterval(x);
+        countDownDate = new Date(now + 10 * 24 * 60 * 60 * 1000).getTime(); // reset to be 10 days ahead
+        document.getElementById("days").innerHTML = "10"; // set days to 10
         document.getElementById("hours").innerHTML = "00";
         document.getElementById("minutes").innerHTML = "00";
         document.getElementById("seconds").innerHTML = "00";
     }
-    
     //the 1000 is the interaval which is in millisecond(1 second)
 },1000);
